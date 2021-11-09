fcd95963a6d727ad2ef2afb83299077e558661c5
JayKays <kveen19@gmail.com>
2021-11-09 17:36:03 +0100

    Added filters

--- Dynamics_Modelling/Data_preperation/visualize_data.py
+++ Dynamics_Modelling/Data_preperation/visualize_data.py
@@ -2,7 +2,11 @@
 from matplotlib import pyplot as plt
 
 import numpy as np
+from filering import lowpass_fft, butter_lowpass
 
+STATES = ["time", "Force-x", "Force-y", "Force-z", \
+    "Torque-x", "Torque-y", "Torque-z", "Pos-x", "Pos-y", \
+        "Pos-z", "Vel-x", "Vel-y", "Vel-z", "Angular-x", "Angular-y", "Angular-z"]
 
 def plot_data(data_arr, idx):
     plt.figure()
@@ -40,20 +44,20 @@ def plot_2D_pos(data_arr):
     plt.figure()
     plt.plot(data[:,7], data[:,8])
 
-def plot_Force(data_arr):
+def plot_Force(data_arr, force_idx = [1,2,3]):
 
     plt.figure()
 
     plt.subplot(131)
-    plt.plot(data_arr[:,1])
+    plt.plot(data_arr[:,force_idx[0]])
     plt.title("Force-x")
 
     plt.subplot(132)
-    plt.plot(data_arr[:,2])
+    plt.plot(data_arr[:,force_idx[1]])
     plt.title("Force-y")
 
     plt.subplot(133)
-    plt.plot(data_arr[:,3])
+    plt.plot(data_arr[:,force_idx[2]])
     plt.title("Force-z")
 
 def plot_end_pos(data_arr):
@@ -72,19 +76,39 @@ def plot_end_pos(data_arr):
     plt.plot(data_arr[:,9])
     plt.title("z")
 
+def plot_filter_comp(data_arr, filtered_data, idx):
+
+    num_plots = len(idx)
+
+    plt.figure()
+
+    for n in range(num_plots):
+        plt.subplot(1, num_plots, n+1)
+        plt.plot(data_arr[:,idx[n]])
+        plt.plot(filtered_data[:,idx[n]])
+        plt.title(STATES[idx[n]])
+
+    plt.legend(["data", "filtered"])
+
+
+def plot_states(data_arr, idx):
+
+    num_plots = len(idx)
+
+    plt.figure()
+
+    for n in range(num_plots):
+        plt.subplot(1, num_plots, n+1)
+        plt.plot(data_arr[:,idx[n]])
+        plt.title(STATES[idx[n]])
+
+    plt.legend(["data", "filtered"])
+
 if __name__ == "__main__":
 
-    data = np.load("processed_data/stand_still.npy")
+    data = np.load("processed_data/something1.npy")
+    data = np.load("processed_data/VIC_data.npy")[:1800,:]
 
-    
-    # plot_data(data, 9)
-    # plot_data(data, 0)
-    # visualize_states(data[2000:7000,:])
-    visualize_states(data)
-    plot_2D_pos(data)
-    plot_Force(data)
-    plot_end_pos(data)
-    # plt.show()
-    # data = np.load("processed_data/alternating.npy")
-    # visualize_states(data[1000:3600,:])
+    plot_filter_comp(data, lowpass_fft(data.copy(), fs = 100, cutoff=6, idx=[1,2,3]), idx=[1,2,3])
+
     plt.show()
